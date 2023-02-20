616bbfb756a670481655bc039a577e37713a67cf
iamnabink <nabraj@supremeit.net>
2023-02-21 01:22:32 +0545

    Code refactored

--- lib/src/esewa_service.dart
+++ lib/src/esewa_service.dart
@@ -12,7 +12,10 @@ class _EsewaService {
         context,
         MaterialPageRoute(builder: (context) => WalletPage(eSewaConfig)),
       );
-      return await Future.delayed(const Duration(milliseconds: 500), () => result);
+      // Wait for the user to return from the Esewa payment screen before closing any dialogs
+      // This delay should give enough time for the success/failure dialog (if any) to appear and prevent it from closing prematurely.
+      return await Future.delayed(
+          const Duration(milliseconds: 500), () => result);
     } catch (e) {
       return EsewaPaymentResult(error: 'Payment Failed or Cancelled!');
     }
